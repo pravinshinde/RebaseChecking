cf14006e12d4610cd44eb5cd2fd6160bfe05cd94
Pravin Shinde <pravin.shinde@infostretch.com>
2018-08-08 12:02:23 +0530

    Add message  Child Branch

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="hello">
-    <h1>Parent Updated</h1>
-    <h1>Child Branch - 4</h1>
-    <h1>Child Branch - 2</h1>
-    <h1>Child Branch - 1</h1>
-    <h1>Child Branch - New 2</h1>
-    <h1>Child Branch - New 1</h1>
+      <h1>Child Branch - New 1</h1>
     <h1>{{ msg }}</h1>
     <p>
       For guide and recipes on how to configure / customize this project,<br>
